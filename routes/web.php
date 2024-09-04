e9fcaa28477df57b74a56a37585132f361e3d77c
Rifqiandy27 <rifqiassasin8@gmail.com>
2024-09-04 09:51:12 +0700

    Praktikum 1 - Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+ return 'World';
+});
+
+Route::get('/selamat datang', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2241760065 dan Moch. Rifqi Andy Setyawan';
+});
+
+//route parameters
+Route::get('/user/{MochRifqiAndySetyawan}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel:'. $id;
+});
+
+//optional parameters
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
 });
